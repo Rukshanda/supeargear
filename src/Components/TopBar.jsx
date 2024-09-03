721e6157490590f5004bdaf8e3aaabf2428f9ea1
Rukshanda <rukshandanadir@gmail.com>
2024-09-03 19:21:31 +0500

    new-files

---
+++ src/Components/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogoBar from './TopBar/LogoBar'
+
+function TopBar() {
+    return (
+        <div>
+            <LogoBar/>
+        </div>
+    )
+}
+
+export default TopBar
---
+++ src/Components/TopBar/LogoBar.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function LogoBar() {
+    return (
+        <div className='max-w-screen-xl'>
+           <div className="logo">
+            <img src="/assets/logo.webp" alt="" />
+           </div>
+        </div>
+    )
+}
+
+export default LogoBar
